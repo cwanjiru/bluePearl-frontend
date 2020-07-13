aa584aad1566439dcd95b675deaa1ab5b3c1905c
christine <gacuirichristine@gmail.com>
2020-07-13 12:42:43 +0300

    Register DealsComponent.

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './homepage.component';
+import { DealsComponent } from './deals/deals.component';
 
 
 
 @NgModule({
-  declarations: [HomepageComponent],
+  declarations: [HomepageComponent, DealsComponent],
   imports: [
     CommonModule
   ]
