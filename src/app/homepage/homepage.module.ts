d16e7ef51aa8cf2d8c9d461ceb8089a17e34b6be
christine <gacuirichristine@gmail.com>
2020-07-14 17:23:52 +0300

    Register ModalSearchComponent.

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './homepage.component';
 import { DealsComponent } from './deals/deals.component';
 import { CoreModule } from '../core/core.module';
+import { ModalSearchComponent } from './modal-search/modal-search.component';
 
 
 
 @NgModule({
-  declarations: [HomepageComponent, DealsComponent],
+  declarations: [HomepageComponent, DealsComponent, ModalSearchComponent],
   imports: [
     CommonModule,
     CoreModule,
