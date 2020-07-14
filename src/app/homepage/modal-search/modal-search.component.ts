eddfd4cc4c9a433c65817aebda4d54ae2afbd002
christine <gacuirichristine@gmail.com>
2020-07-14 17:22:52 +0300

    Generate ModalSearchComponent.

---
+++ src/app/homepage/modal-search/modal-search.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bpl-modal-search',
+  templateUrl: './modal-search.component.html',
+  styles: [
+  ]
+})
+export class ModalSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
