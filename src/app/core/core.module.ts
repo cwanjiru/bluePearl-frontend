74985861f0eae65fe1bef1b4a32de119845f2f37
christine <gacuirichristine@gmail.com>
2020-07-14 11:48:44 +0300

    Register HeaderComponent to CoreModule.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './sections/header/header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [
     CommonModule
   ]
