ac5b539488e5eee4c1f98d12e2c2f6b665909004
christine <gacuirichristine@gmail.com>
2020-07-14 12:00:23 +0300

    Add CoreModule to imports array of HomepageModule.

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './homepage.component';
 import { DealsComponent } from './deals/deals.component';
+import { CoreModule } from '../core/core.module';
 
 
 
 @NgModule({
   declarations: [HomepageComponent, DealsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    CoreModule,
   ]
 })
 export class HomepageModule { }
