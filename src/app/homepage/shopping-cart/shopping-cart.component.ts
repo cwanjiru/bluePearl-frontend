38c7915fbb91f4a729b996e54726d64d64ed4950
christine <gacuirichristine@gmail.com>
2020-07-14 17:30:36 +0300

    Generate ShoppingCartComponent.

---
+++ src/app/homepage/shopping-cart/shopping-cart.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bpl-shopping-cart',
+  templateUrl: './shopping-cart.component.html',
+  styles: [
+  ]
+})
+export class ShoppingCartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
