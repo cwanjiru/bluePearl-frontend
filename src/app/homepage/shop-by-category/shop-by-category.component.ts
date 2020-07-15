1d4ef9ce99cdc228d7e1fe0b25a34b785b67332d
christine <gacuirichristine@gmail.com>
2020-07-15 12:08:04 +0300

    Generate ShopByCategoryComponent.

---
+++ src/app/homepage/shop-by-category/shop-by-category.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bpl-shop-by-category',
+  templateUrl: './shop-by-category.component.html',
+  styles: [
+  ]
+})
+export class ShopByCategoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
