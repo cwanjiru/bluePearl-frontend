0b50285557c43e8723a8be2f9dc99a89bae87214
christine <gacuirichristine@gmail.com>
2020-07-15 11:29:10 +0300

    Register SliderComponent.

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -5,11 +5,12 @@ import { DealsComponent } from './deals/deals.component';
 import { CoreModule } from '../core/core.module';
 import { ModalSearchComponent } from './modal-search/modal-search.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { SliderComponent } from './slider/slider.component';
 
 
 
 @NgModule({
-  declarations: [HomepageComponent, DealsComponent, ModalSearchComponent, ShoppingCartComponent],
+  declarations: [HomepageComponent, DealsComponent, ModalSearchComponent, ShoppingCartComponent, SliderComponent],
   imports: [
     CommonModule,
     CoreModule,
