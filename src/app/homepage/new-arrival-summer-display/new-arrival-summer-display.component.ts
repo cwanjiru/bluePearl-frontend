aee2f32d319ff5631383124c18bc876a322ff2aa
christine <gacuirichristine@gmail.com>
2020-07-16 12:59:10 +0300

    Generate NewArrivalSummerDisplayComponent.

---
+++ src/app/homepage/new-arrival-summer-display/new-arrival-summer-display.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bpl-new-arrival-summer-display',
+  templateUrl: './new-arrival-summer-display.component.html',
+  styles: [
+  ]
+})
+export class NewArrivalSummerDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
