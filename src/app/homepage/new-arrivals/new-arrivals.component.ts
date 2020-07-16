267b9535e452e4def0a2a8cd4d711035f8445141
christine <gacuirichristine@gmail.com>
2020-07-16 11:43:08 +0300

    Generate NewArrivalsComponent.

---
+++ src/app/homepage/new-arrivals/new-arrivals.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bpl-new-arrivals',
+  templateUrl: './new-arrivals.component.html',
+  styles: [
+  ]
+})
+export class NewArrivalsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
