645d68de68d3887c90d50588314d9aa0e58956b0
christine <gacuirichristine@gmail.com>
2020-07-16 11:16:14 +0300

    Register ShopByCategorySamplesComponent.

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -7,11 +7,12 @@ import { ModalSearchComponent } from './modal-search/modal-search.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { SliderComponent } from './slider/slider.component';
 import { ShopByCategoryComponent } from './shop-by-category/shop-by-category.component';
+import { ShopByCategorySamplesComponent } from './shop-by-category-samples/shop-by-category-samples.component';
 
 
 
 @NgModule({
-  declarations: [HomepageComponent, DealsComponent, ModalSearchComponent, ShoppingCartComponent, SliderComponent, ShopByCategoryComponent],
+  declarations: [HomepageComponent, DealsComponent, ModalSearchComponent, ShoppingCartComponent, SliderComponent, ShopByCategoryComponent, ShopByCategorySamplesComponent],
   imports: [
     CommonModule,
     CoreModule,
