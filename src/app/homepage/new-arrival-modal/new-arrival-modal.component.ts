588dfcc17026b9dd57e03fcc421b4224a343f426
christine <gacuirichristine@gmail.com>
2020-07-16 12:10:32 +0300

    Generate NewArrivalModalComponent.

---
+++ src/app/homepage/new-arrival-modal/new-arrival-modal.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bpl-new-arrival-modal',
+  templateUrl: './new-arrival-modal.component.html',
+  styles: [
+  ]
+})
+export class NewArrivalModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
