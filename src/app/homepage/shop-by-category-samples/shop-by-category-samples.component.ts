bbd5626abde55ac52222896c2fbafb68d94c1f1b
christine <gacuirichristine@gmail.com>
2020-07-16 11:13:26 +0300

    Generate ShopByCategorySamplesComponent.

---
+++ src/app/homepage/shop-by-category-samples/shop-by-category-samples.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bpl-shop-by-category-samples',
+  templateUrl: './shop-by-category-samples.component.html',
+  styles: [
+  ]
+})
+export class ShopByCategorySamplesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
