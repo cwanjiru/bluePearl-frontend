638462c0bb9cbee39ef7bd3cc58e0752604a158b
christine <gacuirichristine@gmail.com>
2020-07-17 16:16:28 +0300

    Implement fetchWomenCatalog method that fetches all catalogs that have a category value of women,fetchMenCatalog method that fetches all catalogs that have a categoryt value of men,fetchKidsCatalog method that fetches all catalogs that have a category value of kids and setUpCatalog method that builds catalog items.

--- src/app/core/catalog.service.ts
+++ src/app/core/catalog.service.ts
@@ -1,9 +1,90 @@
 import { Injectable } from '@angular/core';
+import { CoreModule } from './core.module';
+import { Catalog } from './catalog';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: CoreModule
 })
-export class CatalogService {
 
-  constructor() { }
+export class CatalogService {
+  private allCatalogs: Array<Catalog> =[];
+
+  constructor() {
+    this.setUpCatalog();
+   }
+
+  fetchWomenCatalog = ():Array<Catalog> =>{
+    let womenCatalog = [];
+    for(let i=0; i<this.allCatalogs.length;i++){
+      if(this.allCatalogs[i]['property'] == 'women'){
+        womenCatalog.push(this.allCatalogs[i]);
+      }
+    }
+    return womenCatalog;
+  }
+
+  fetchMenCatalog = (): Array<Catalog> =>{
+    let menCatalog = [];
+    for(let i=0; i<this.allCatalogs.length; i++){
+      if(this.allCatalogs[i]['property'] == 'men'){
+        menCatalog.push(this.allCatalogs[i]);
+      } 
+    }
+      return menCatalog;
+  }
+  
+  fetchKidsCatalog = ():Array<Catalog> =>{
+    let kidsCatalog = [];
+    for(let i=0; i<this.allCatalogs.length; i++){
+      if(this.allCatalogs[i]['property'] == 'kids'){
+        kidsCatalog.push(this.allCatalogs[i]);
+      }
+    }
+    return kidsCatalog;
+  }
+
+  private setUpCatalog = () => {
+    this.allCatalogs.push(
+      new Catalog(
+        'women',
+        [
+          'clothing', 
+          'shoes',
+          'dresses',
+          'bags',
+          'accessories',
+          'sunglasses',
+          'denim',
+          'boots',
+          'jewelry'
+        ]
+      ));
+      this.allCatalogs.push(
+        new Catalog(
+          'men',
+          [
+            'clothing',
+            'shoes',
+            'coats & jackets',
+            'bags',
+            'accessories',
+            'sunglasses',
+            'denim',
+            'sneakers',
+            'watches'
+          ]
+        ));
+        this.allCatalogs.push(
+          new Catalog(
+            'kids',
+            [
+              'clothing',
+              'shoes',
+              'accessories',
+              'sunglasses',
+              'denim',
+              'boots'
+            ]
+          ));
+  }
 }
