6490ebf0eeed70c8e0e422f97ad1f5fe0be38604
christine <gacuirichristine@gmail.com>
2020-07-17 11:52:38 +0300

    Generate LatestInBlogComponent.

---
+++ src/app/homepage/latest-in-blog/latest-in-blog.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bpl-latest-in-blog',
+  templateUrl: './latest-in-blog.component.html',
+  styles: [
+  ]
+})
+export class LatestInBlogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
